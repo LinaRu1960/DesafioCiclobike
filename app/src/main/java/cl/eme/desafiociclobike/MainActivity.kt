95a910a2dfe2beca104c649d41be6584f4f754f0
paulina <gofepal.ruiz@gmail.com>
2020-12-23 10:31:40 +0100

    Iniciando Desafio CicloBike 2.0

--- app/src/main/java/cl/eme/desafiociclobike/MainActivity.kt
+++ app/src/main/java/cl/eme/desafiociclobike/MainActivity.kt
@@ -7,15 +7,26 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import cl.eme.desafiociclobike.databinding.ActivityMainBinding
 
 /*
+Desafío - Ciclobike
 [X] 1. Crear el proyecto Android de cero, compatible con Kotlin.
 [X] 2. Convertir las clases Java que vienen de recursos en el proyecto a clases Kotlin.
 [X] 3. Crear las listas en base a las clases entregadas para comenzar el proyecto.
-[] 4. Crear una interfaz gráfica que cumpla con los requerimientos.
-[] 5. Hacer un buen uso del null safety mientras se desarrolla el proyecto.
-[] 6. Crear el filtro especificado usando la sentencia when.
+[X] 4. Crear una interfaz gráfica que cumpla con los requerimientos.
+[X] 5. Hacer un buen uso del null safety mientras se desarrolla el proyecto.
+[X] 6. Crear el filtro especificado usando la sentencia when.
+
+ Desafío - Ciclobike 2.0
+[] 1. Actualizar la Activity del desafío 1, usando los view bindings.
+[] 2. Actualizar los listener de los botones a las funciones lambdas de kotlin.
+[] 3. Modificar la interfaz a lo nuevo requerido que incluye spinner
+[] 4. Ahora usar la función filter para filtrar.
+[] 5. Agregar botón que invierte el orden de la lista.
+[] 6. Agregar EditText más botón filtrar, que filtra según lo escrito en el editText.
+[] 7. Hacer que el filtro anterior no sea sensible a mayúsculas y minúsculas.
+[] 8. Hacer otro filtro igual que el anterior, pero que filtre por el nombre de la ciclovía.
+[] 9. Hacer otro filtro igual que el anterior, pero donde se ingrese un número y se filtrara si la
+      comuna tiene un largo mayor a ese que se ingresó, además la app no debe caerse si se ingresa un valor no numérico.
  */
-
-
 
 class MainActivity : AppCompatActivity() {
     val cicloviaAdapter = CicloviaAdapter()
